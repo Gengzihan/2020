08f5ce91cef39e7f0a6fffc3621037f898891cfd
Gengzihan <3098358504@qq.com>
2019-10-15 20:00:04 +0800

    日出日落

--- 正式课1/weak5/day1/渐显轮播图/js/index.js
+++ 正式课1/weak5/day1/渐显轮播图/js/index.js
@@ -1,4 +1,104 @@
-function getData(){
-    $.ajax
-}
+var banner = (function () {
+    let idSelector = '';
+    let $box = null,
+        $ul = null,
+        $lis = null,
+        $tipBox = null,
+        $tips = null,
+        $leftBtn = null,
+        $rightBtn = null;
+    var n = 0,
+        timer = null; // n 控制了全局图片对应的索引   
+    function initEle() {
+        $box = $(idSelector);
+        $ul = $box.find('.img_box ul'),
+            $lis = $box.find('.img_box ul li'),
+            $tipBox = $box.find('.tip_box'),
+            $tips = $tipBox.children('.tip'),
+            $leftBtn = $box.find('.left_btn'),
+            $rightBtn = $box.find('.right_btn');
 
+        $lis.eq(0).show().siblings().hide();
+    }
+
+    function getData() {
+        $url.ajax({
+            url: './data.json',
+            success: function (data) {
+                //
+                render(data);
+                initEle();
+                autoMove();
+            },
+            error: function () {
+                alert('失败')
+            }
+        })
+    }
+
+    function render(data) {
+        let str = '',
+            tipStr = '';
+        data.forEach((item, index) => {
+            str += `<li>
+                <img src="${item.img}" alt="">
+                <p>${item.desc}</p>
+            </li>`;
+            tipStr += (index == 0 ? `<span class="tip current"></span> ` : `<span class="tip"></span> `)
+        })
+        $ul.html(str)
+        $tipBox.html(tipStr);
+    }
+
+    function move() {
+        n++;
+        if (n > $lis.length - 1) {
+            n = 0;
+        }
+        $lis.eq(n).show().css({
+            opacity: 0
+        }).animate({
+            opacity: 1
+        }, 300).siblings().animate({
+            opacity: 0
+        }, 300, function () {
+            $lis.eq(n).siblings().hide();
+        })
+    }
+
+    function autoMove() {
+        timer = setInterval(() => {
+            move();
+        }, 2000)
+    }
+
+    function autoFocus() {
+        $tips.eq(n).addClass('current').siblings().removeClass('current');
+    }
+
+    function eventBined() {
+        $box.on('mouseenter', function () {
+            clearInterval(timer)
+        })
+        $box.on('mouseenter', function () {
+            autoMove();
+        })
+        $leftBtn.on('click', function () {
+            n--;
+            if (n < 0) {
+                n = $lis.length - 1;
+            }
+        })
+        $rightBtn.on('click', function () {
+            move();
+        })
+    }
+    return {
+        init(id) {
+            idSelector = id;
+            getData();
+            initEle();
+        }
+    }
+})()
+banner.init('#box');
